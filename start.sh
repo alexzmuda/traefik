fea23fac167b833d7aa6eca965f83d6a29b0cca0
alexz <alex@codateam.com>
2020-11-08 20:55:00 +0100

    setup

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+docker network remove myroutemap
 docker network create myroutemap
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up --force-recreate
